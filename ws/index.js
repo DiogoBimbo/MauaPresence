b5deaf8d6913c29582f8a88acf57d96236d10890
RivianSoldier <bilzims.sg@gmail.com>
2023-05-05 12:48:06 -0300

    preparacao do ambiente de desenvolvimento

---
+++ ws/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+
+// middlewares
+app.use(morgan("dev"));
+
+// variables
+app.set("port", 8000);
+
+app.listen(app.get("port"), () => {
+  console.log(`WS escutando na porta ${app.get("port")}`);
+});
