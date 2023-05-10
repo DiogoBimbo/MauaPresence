6fe60caf676517b65affe827a75007d7f6c3e374
RivianSoldier <bilzims.sg@gmail.com>
2023-05-10 19:32:28 -0300

    Criacao da rota de login para adm e preparacao de rotas de login para alunos e professores

--- ws/src/routes/adm.routes.js
+++ ws/src/routes/adm.routes.js
@@ -1,6 +1,9 @@
+const bcrypt = require("bcrypt");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
-//const Admin = require('../models/administrador');
+
+const Admin = require("../models/administrador");
 
 // router.post('/', async (req,res) => {
 //     try {
@@ -11,4 +14,78 @@ const router = express.Router();
 //     }
 // });
 
+// rota login usando jwt e bcrypt
+router.post("/administrador/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const admin = await Admin.findOne({ email });
+    if (!admin) {
+      return res.status(401).json({ error: "Email ou senha inválidos" });
+    }
+    const passwordCheck = await bcrypt.compare(password, admin.password);
+    if (!passwordCheck) {
+      return res.status(401).json({ error: "Email ou senha inválidos" });
+    }
+    const token = jwt.sign({ id: admin._id }, "chave secreta", {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 module.exports = router;
+
+// --------------------------------------------------------------------------------
+
+// npm install jsonwebtoken
+//npm install bcrypt
+
+// rota login alunos
+// const Aluno = require("../models/aluno");
+
+// router.post("/aluno/login", async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+//     const aluno = await Aluno.findOne({ email });
+//     if (!aluno) {
+//       return res.status(401).json({ error: "Email ou senha inválidos" });
+//     }
+//     const passwordCheck = await bcrypt.compare(password, aluno.password);
+//     if (!passwordCheck) {
+//       return res.status(401).json({ error: "Email ou senha inválidos" });
+//     }
+//     const token = jwt.sign({ id: aluno._id }, "chave secreta", {
+//       expiresIn: "1h",
+//     });
+//     res.json({ token });
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
+
+// ----------------------------------------------------------------------------
+
+// rota login professores
+// const Professor = require("../models/professor");
+
+// router.post("/aluno/login", async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+//     const professor = await Professor.findOne({ email });
+//     if (!professor) {
+//       return res.status(401).json({ error: "Email ou senha inválidos" });
+//     }
+//     const passwordCheck = await bcrypt.compare(password, professor.password);
+//     if (!passwordCheck) {
+//       return res.status(401).json({ error: "Email ou senha inválidos" });
+//     }
+//     const token = jwt.sign({ id: professor._id }, "chave secreta", {
+//       expiresIn: "1h",
+//     });
+//     res.json({ token });
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
