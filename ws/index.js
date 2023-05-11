ed98bcec37897b90cccca8c1b45695ad3985884f
Diogo Carvalho <diogobernardini2003@gmail.com>
2023-05-11 18:20:24 -0300

    Criação das rotas de cadastro dos cursos

--- ws/index.js
+++ ws/index.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const session = require('express-session');
+const flash = require('connect-flash');
 const exphbs = require('express-handlebars');
 const app = express();
 const bodyParser = require('body-parser');
-const admin = require('./src/routes/admin.routes')
+const admin = require('./src/routes/admin.routes');
 require('./database');
 
 //Configs
+
   // Template Engine
   app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
-  app.set('view engine', 'handlebars')
+  app.set('view engine', 'handlebars');
+
+  // Sessão
+  app.use(session({
+    secret: "curiosidade",
+    resave: true,
+    saveUninitialized: true
+  }));
+  app.use(flash());
 
   // Middlewares
+  app.use((req, res, next) =>{
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+  });
   app.use(morgan('dev'));
   app.use(express.json());  
   app.use(cors());
