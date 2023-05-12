955b192f8d247c43734935f93eec2a8fea349542
RivianSoldier <bilzims.sg@gmail.com>
2023-05-12 20:49:53 -0300

    criação da rota de cadastro de professores

--- ws/src/routes/admin.routes.js
+++ ws/src/routes/admin.routes.js
@@ -114,8 +114,82 @@ router.get("/alunos", async (req, res) => {
 });
 
 router.get("/professores", async (req, res) => {
-  res.send("Página de professores");
+  res.render("admin/professores");
 });
+
+router.get("/professores/cadastrar", async (req, res) => {
+  res.render("admin/cadastrar_professores");
+});
+
+router.post("/professores/novo", async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(req.body.senha, salt);
+  const novoProfessor = {
+    ra: req.body.ra,
+    nome_completo: req.body.nome_completo,
+    email: req.body.email,
+    senha: hash
+  };
+
+//   // Validação dos campos usando Joi
+  const schema = Joi.object({
+    ra: Joi.string().required().messages({
+      "any.required": "O campo RA do professor é obrigatório",
+      "string.empty": "Por favor, informe um valor para o RA do professor",
+    }),
+    nome_completo: Joi.string().required().messages({
+      "any.required": "O campo nome do professor é obrigatório",
+      "string.empty": "Por favor, informe o nome do professor.",
+    }),
+    email: Joi.string().email().required().messages({
+      "any.required": "O campo email do professor é obrigatório",
+      "string.empty": "Por favor, informe um valor para o email do professor",
+      "string.email": "Por favor, informe um email válido",
+    }),
+    senha: Joi.string().required().messages({
+      "any.required": "O campo senha do professor é obrigatório",
+      "string.empty": "Por favor, informe um valor para a senha do professor",
+    })
+  });
+
+  const { error } = schema.validate(novoProfessor);
+  if (error) {
+    req.flash(
+      "error_msg",
+      "Erro ao cadastrar professor: " + error.details[0].message
+    );
+    res.redirect("/admin/professores/cadastrar");
+    return;
+  }
+
+//   // Verifica se já existe algum professor cadastrado com o mesmo RA
+  const professorExistente = await Professor.findOne({
+    $or: [
+      { ra: novoProfessor.ra },
+    ],
+  });
+
+  if (professorExistente) {
+    req.flash("error_msg", "Já existe um professor cadastrado com este RA");
+    res.redirect("/admin/professores/cadastrar");
+    return;
+  }
+
+  new Professor(novoProfessor)
+    .save()
+    .then(() => {
+      req.flash("success_msg", "Professor cadastrado com sucesso");
+      res.redirect("/admin/professores");
+    })
+    .catch((err) => {
+      req.flash("error_msg", "Erro ao cadastrar professor");
+      res.redirect("/admin/professores/cadastrar");
+    });
+});
+
+
+
+
 
 router.get("/aulas", async (req, res) => {
   res.send("Página de cursos");
