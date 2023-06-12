8cbb4a0b21e3fa11e20deaccf452c0295bddbc5d
RivianSoldier <bilzims.sg@gmail.com>
2023-06-12 15:17:31 -0300

    Adição de funcionalidade para redefinir senha do professor na tela de login e no dashboard

--- ws/src/models/aluno.js
+++ ws/src/models/aluno.js
@@ -1,52 +1,52 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const alunoSchema = new Schema({
-    ra: {
-        type: String,
-        required: true
-    },
-    nome_completo: {
-        type: String,
-        required: true
-    },
-    semestre: {
-        type: Number,
-        required: true
-    },
-    grupo: {
-        type: String,
-        required: true
-    },
-    turma: {
-        type: String,
-        required: true
-    },
-    lab: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    senha: {
-        type: String,
-        required: true
-    },
-    id_curso: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Curso',
-        required: true
-    },
-    resetToken: {
-        type: String,
-        default: null
-      },
-      resetTokenExpiration: {
-        type: Date,
-        default: null
-      }
+  ra: {
+    type: String,
+    required: true,
+  },
+  nome_completo: {
+    type: String,
+    required: true,
+  },
+  semestre: {
+    type: Number,
+    required: true,
+  },
+  grupo: {
+    type: String,
+    required: true,
+  },
+  turma: {
+    type: String,
+    required: true,
+  },
+  lab: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  senha: {
+    type: String,
+    required: true,
+  },
+  id_curso: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Curso",
+    required: true,
+  },
+  resetToken: {
+    type: String,
+    default: null,
+  },
+  resetTokenExpiration: {
+    type: Date,
+    default: null,
+  },
 });
 
-module.exports = mongoose.model('Aluno', alunoSchema);
+module.exports = mongoose.model("Aluno", alunoSchema);
